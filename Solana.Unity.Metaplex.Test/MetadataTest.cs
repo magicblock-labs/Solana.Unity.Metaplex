52d574bfd88c7ca9311f260b1ea10fee35f483a3
Gabriele Picco <piccogabriele@gmail.com>
2023-04-15 12:02:03 +0200

    :bug: Fix deserializing NFTs with multiple creators

--- Solana.Unity.Metaplex.Test/MetadataTest.cs
+++ Solana.Unity.Metaplex.Test/MetadataTest.cs
@@ -261,5 +261,52 @@ namespace Solana.Unity.Metaplex.Test
                 }
             }
         }
+        
+        [TestMethod]
+        public async Task TestGetMetadata() 
+        {
+            var client = Solana.Unity.Rpc.ClientFactory.GetClient(Solana.Unity.Rpc.Cluster.MainNet);
+
+            string pk = "6s8xhWNqQQsHYJ3UAdxWSNMfaQ4fZ53kPsNt9tiqgdct";
+
+            Console.WriteLine("### Get Metadata example ###");
+            Console.WriteLine("Getting account {0}", pk );
+            
+            var account = await MetadataAccount.GetAccount( client, new PublicKey(pk));
+
+            Console.WriteLine( $"Owner: {account.owner}");
+            Console.WriteLine( $"Authority key: {account.updateAuthority}");
+            Console.WriteLine( $"Mint key: {account.mint}");
+            Console.WriteLine( $"Name: {account.metadata.name}");
+            Console.WriteLine( $"Symbol: {account.metadata.symbol}");
+            Console.WriteLine( $"Uri: {account.metadata.uri}");
+            Console.WriteLine( $"SellerFeeBasisPoints: {account.metadata.sellerFeeBasisPoints}");
+
+            Console.WriteLine( $"---Creators---");
+            foreach( Creator c in account.metadata.creators)
+            {
+                Console.WriteLine( $"Creator Key: {c.key}");
+                Console.WriteLine( $"Creator Share: {c.share}");
+                Console.WriteLine( $"Creator is verified: {c.verified}");
+            }
+
+            Console.WriteLine(  "-------Metadata-------");
+            Console.WriteLine($"Name: {account.offchainData.name}");
+            Console.WriteLine($"Description: {account.offchainData.description}");
+            Console.WriteLine($"Symbol: {account.offchainData.symbol}");
+            Console.WriteLine($"Collection: {account.offchainData.collection}");
+            Console.WriteLine($"Default Image: { account.offchainData.default_image }" );
+            Console.WriteLine($"Animation url: {account.offchainData.animation_url}");
+
+            foreach (var attribute in account.offchainData.attributes)
+            {
+                if(attribute != null)
+                    Console.WriteLine($"Attribute: { attribute.trait_type } | { attribute.value }");
+
+            }
+
+            Console.WriteLine ( "------------------");
+        }
+        
     }
 }
--- Solana.Unity.Metaplex/Metadata Program/Account/MetadataAccount.cs
+++ Solana.Unity.Metaplex/Metadata Program/Account/MetadataAccount.cs
@@ -117,7 +117,7 @@ namespace Solana.Unity.Metaplex.NFT.Library
                 uint sellerFee = binData.GetU16(MetadataPacketLayout.feeBasisOffset);
 
                 //bool hasCreators = binData.GetBool(MetadataPacketLayout.creatorSwitchOffset);
-                byte numOfCreators = binData.GetU8(MetadataPacketLayout.creatorsCountOffset);
+                int numOfCreators = (int) binData.GetU32(MetadataPacketLayout.creatorsCountOffset + 1);
 
                 IList<Creator> creators = null;
                 Uses usesInfo = null;
